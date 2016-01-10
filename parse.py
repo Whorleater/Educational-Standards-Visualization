8e91c82b696eff16880128bcf382bc55544fcec9
Peixian Wang <peixian@plains.guru>
2016-01-09 22:25:34 -0600

    Finished Parser
    Need to now turn the parser into a tree structure of json or something similar.

--- parse.py
+++ parse.py
@@ -5,27 +5,18 @@ from pprint import pprint
 hasChild = 'http://purl.org/gem/qualifiers/hasChild'
 description = 'http://purl.org/dc/terms/description'
 
-def printNode(data, node):
+def printNode(data, node, level):
     #print the current node's description
     print("")
-    print(data[node][description][0]["value"])
+    print("{}{}".format("    "*level, data[node][description][0]["value"]))
     #get the current node's children if they exist, returns None if they don't
     children = data[node].get(hasChild)
     #if the children exists, then loop through them and get their children
     if (children != None):
         for item in children:
             node = item["value"]
-            printNode(data, node)
-    # if children is not None:
-    #     #pprint(data[node][hasChild])
-    #     #print("This node has {} children".format(len(children)))
-    #     for item in
-    #     for i in range(0, len(children)):
-    #         #print("\nAt node {} i is: {}".format(data[node][description][0]["value"], i))
-    #         print("data[{}][hasChild][{}]['value']".format(node, i))
-    #         node = data[node][hasChild][i]["value"]
-    #         printNode(data, node)
-
+            level += 1
+            printNode(data, node, level)
 #t1-s1
 t1s1 = pd.read_csv("data/t1-s1.csv")
 uniqueSubjectNotations = pd.unique(t1s1.subjectNotation.ravel())
@@ -42,6 +33,6 @@ with open("data/t1.json") as t1_file:
 #pprint(t1[t1["http://asn.jesandco.org/resources/D10003B9"]['http://purl.org/gem/qualifiers/hasChild'][0]["value"]]) 
 
 t1root = "http://asn.jesandco.org/resources/D10003B9"
-printNode(t1, t1root)
+printNode(t1, t1root, 0)
 #t1[t1root][hasChild][0]["value"]
 
