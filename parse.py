df84784249f60be074ea8db620ff96fff91c3fef
Peixian Wang <peixian@plains.guru>
2016-01-10 21:59:57 -0600

    Subjects now have objects
    The subjects each are containted within the individual objects for easy tree making, gitignore amended for a few stragglers

--- parse.py
+++ parse.py
@@ -4,19 +4,56 @@ from pprint import pprint
 
 hasChild = 'http://purl.org/gem/qualifiers/hasChild'
 description = 'http://purl.org/dc/terms/description'
+listID = 'http://purl.org/ASN/schema/core/listID'
+statementNotation = 'http://purl.org/ASN/schema/core/statementNotation'
+educationLevel = 'http://purl.org/dc/terms/educationLevel'
+subject = 'http://purl.org/dc/terms/subject'
+
+class standards:
+    ID = ''
+    children = []
+    description = ''
+    subject = ''
+    educationLevel = []
+    depth = -1
+    def __init__(self, ID, description, children, subject, educationLevel, depth):
+        self.ID = ID
+        self.description = description
+        self.children = children
+        self.subject = subject
+        self.educationLevel = educationLevel
+        self.depth = depth
+    
 
 def printNode(data, node, level):
-    #print the current node's description
-    print("")
-    print("{}{}".format("    "*level, data[node][description][0]["value"]))
+    currentNodeDescription = str(data[node][description][0]["value"])
+
+    if (data[node].get(listID)):
+        currentNodeID = data[node][listID][0]["value"]
+    elif (data[node].get(statementNotation)): 
+        currentNodeID = data[node][statementNotation][0]["value"]
+    else:
+        currentNodeID = ''
+
     #get the current node's children if they exist, returns None if they don't
     children = data[node].get(hasChild)
+    
+    #get the subject and education level
+    currentNodeSubject = data[node][subject][0]["value"]
+    currentNodeEducation = data[node][educationLevel]
+    #create the node object
+    currentNode = standards(currentNodeID, currentNodeDescription, children, subject, currentNodeEducation, level)
+    #print the current node's description
+    print("")
+    print("{}ID: {}, Description: {}".format("    "*level, currentNode.ID, currentNode.description[:30]))
     #if the children exists, then loop through them and get their children
     if (children != None):
         for item in children:
             node = item["value"]
             level += 1
             printNode(data, node, level)
+            
+
 #t1-s1
 t1s1 = pd.read_csv("data/t1-s1.csv")
 uniqueSubjectNotations = pd.unique(t1s1.subjectNotation.ravel())
