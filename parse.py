6c7929414fb4f33c368e5c4b369f6ad2ca157a3c
Peixian Wang <peixian@plains.guru>
2016-01-11 21:55:52 -0600

    More parsing stuff (nonfunctional)
    Added a little more parsing stuff, it's nonfunctional right now though.

--- parse.py
+++ parse.py
@@ -9,23 +9,25 @@ statementNotation = 'http://purl.org/ASN/schema/core/statementNotation'
 educationLevel = 'http://purl.org/dc/terms/educationLevel'
 subject = 'http://purl.org/dc/terms/subject'
 
+#make a standard 
 class standards:
     ID = ''
     children = []
     description = ''
     subject = ''
     educationLevel = []
-    depth = -1
-    def __init__(self, ID, description, children, subject, educationLevel, depth):
+    uri = ''
+    hasChildren = False
+    def __init__(self, ID, description, children, subject, educationLevel, uri):
         self.ID = ID
         self.description = description
         self.children = children
         self.subject = subject
         self.educationLevel = educationLevel
-        self.depth = depth
-    
+        self.uri = uri
 
-def printNode(data, node, level):
+
+def printNode(l, data, node, level):
     currentNodeDescription = str(data[node][description][0]["value"])
 
     if (data[node].get(listID)):
@@ -37,23 +39,42 @@ def printNode(data, node, level):
 
     #get the current node's children if they exist, returns None if they don't
     children = data[node].get(hasChild)
-    
+
     #get the subject and education level
     currentNodeSubject = data[node][subject][0]["value"]
     currentNodeEducation = data[node][educationLevel]
     #create the node object
-    currentNode = standards(currentNodeID, currentNodeDescription, children, subject, currentNodeEducation, level)
-    #print the current node's description
-    print("")
-    print("{}ID: {}, Description: {}".format("    "*level, currentNode.ID, currentNode.description[:30]))
+    currentNode = standards(currentNodeID, currentNodeDescription, children, currentNodeSubject, currentNodeEducation, node)
+    l.append(currentNode)
+    #print("{}ID: {}, Description: {}".format("    "*level, currentNode.ID, currentNode.description[:30]))
     #if the children exists, then loop through them and get their children
     if (children != None):
         for item in children:
             node = item["value"]
             level += 1
-            printNode(data, node, level)
-            
+            currentNode.hasChildren = True
+            printNode(l, data, node, level)
 
+def writeJson(data, sortedList):
+    for item in sortedList:
+        if (len(item.ID) == 2):
+            jsonObject = data[item.uri]
+            jsonObject[hasChild] = []
+            #pprint(json.dumps(jsonObject))
+            for child in item.children:
+                writeRecursive(data, sortedList, child, jsonObject[hasChild])
+            
+    pprint(json.dumps(jsonObject))
+    
+def writeRecursive(data, sortedList, item, childArray):
+    jsonObject = data[item["value"]]
+    jsonObject[hasChild] = []
+    children = data[item["value"]].get(hasChild)
+    if (children != None):
+        for child in children:
+            writeRecursive(data, sortedList, item, jsonObject[hasChild])
+    childArray.append(json.dumps(jsonObject))
+    
 #t1-s1
 t1s1 = pd.read_csv("data/t1-s1.csv")
 uniqueSubjectNotations = pd.unique(t1s1.subjectNotation.ravel())
@@ -70,6 +91,14 @@ with open("data/t1.json") as t1_file:
 #pprint(t1[t1["http://asn.jesandco.org/resources/D10003B9"]['http://purl.org/gem/qualifiers/hasChild'][0]["value"]]) 
 
 t1root = "http://asn.jesandco.org/resources/D10003B9"
-printNode(t1, t1root, 0)
+
+l = []
+printNode(l, t1, t1root, 0)
+
+# for item in l:
+#     print("{} {}".format(item.ID, item.children))
+
+writeJson(t1, l)
+
 #t1[t1root][hasChild][0]["value"]
 
